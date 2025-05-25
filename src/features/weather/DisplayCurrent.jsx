f1e33c5e0fa247fa2bf6ca0ccf41f7faf4f93219
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-25 12:20:29 +0330

    fix: fix responsive wideCard component on mobile

--- src/features/weather/DisplayCurrent.jsx
+++ src/features/weather/DisplayCurrent.jsx
@@ -15,7 +15,7 @@ export const DisplayCurrent = ({
 }) => {
     return (
         <div className="h-fit w-full rounded-4xl border border-neutral-500/40 bg-gradient-to-t from-sky-600/25 from-45% to-sky-800/10 to-100% px-6 py-5 md:px-12 md:py-10">
-            <div className="flex flex-col lg:flex-row lg:items-center">
+            <div className="flex flex-col gap-1 lg:flex-row lg:items-center">
                 <div className="flex flex-col gap-1 py-4 lg:basis-1/3">
                     <h3 className="gap-2 text-sm font-light text-neutral-500">Your current location</h3>
                     <p className="flex flex-col text-4xl text-neutral-300">
--- src/features/weather/WideCard.jsx
+++ src/features/weather/WideCard.jsx
@@ -2,7 +2,7 @@ export const WideCard = ({ label, leftData, rightData }) => {
     return (
         <div className="flex h-full w-full flex-col justify-between gap-2 rounded-3xl border border-neutral-500/40 bg-white/3 p-6">
             <h4 className="text-md font-light text-orange-400/70">{label}</h4>
-            <div className="flex items-center gap-2">
+            <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
                 <div className="flex gap-2">
                     <div className="flex h-full items-start">
                         <div className="rounded-full border border-neutral-500 bg-white/5 p-1.5 text-neutral-300">
