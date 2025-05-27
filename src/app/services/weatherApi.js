75ec09298942adab4b921683a6699598470c9d53
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-05-27 09:21:36 +0330

    chore: remove getCurrentData endpoint

--- src/app/services/weatherApi.js
+++ src/app/services/weatherApi.js
@@ -4,15 +4,6 @@ const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 const weatherApi = api.injectEndpoints({
     endpoints: (builder) => ({
-        getCurrentData: builder.query({
-            query: (searchQuery) => ({
-                url: '/current.json',
-                params: {
-                    q: searchQuery,
-                    key: WEATHER_API_KEY,
-                },
-            }),
-        }),
         getForecastData: builder.query({
             query: (searchQuery, days = 3) => ({
                 url: '/forecast.json',
@@ -27,9 +18,4 @@ const weatherApi = api.injectEndpoints({
 });
 
 export default weatherApi;
-export const {
-    useGetForecastDataQuery,
-    useLazyGetForecastDataQuery,
-    useGetCurrentDataQuery,
-    useLazyGetCurrentDataQuery,
-} = weatherApi;
+export const { useGetForecastDataQuery, useLazyGetForecastDataQuery } = weatherApi;
